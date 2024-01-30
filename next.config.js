1b1e50263d31dbaf8e49274c6d15b8debd2bc46f
liamtft <liam@tsangs.us>
2024-01-29 21:28:28 -0500

    fix 0.3

--- next.config.js
+++ next.config.js
@@ -41,14 +41,6 @@ const nextConfig = {
         },
       ],
     })
-
-    // shader support
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      exclude: /node_modules/,
-      use: ['raw-loader', 'glslify-loader'],
-    })
-
     return config
   },
 }
