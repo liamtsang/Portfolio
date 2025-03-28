fa7a20df253036fd3dba86c9261ab1cbe6a9cb18
liamtsang <lt1800@nyu.edu>
2025-03-27 22:50:17 -0400

    init

---
+++ src/lib/project-card.svelte
@@ -0,0 +1,77 @@
+<script>
+  import { draggable } from '@neodrag/svelte';
+  let { project, initialAngle, id } = $props()
+
+
+  // TODO: Bring the card to the top of the z-index stack
+  let test = () => {
+    console.log("test")
+  }
+</script>
+
+<div class="drag-wrapper" use:draggable={{ onDragStart: test }}>
+  <div class="project-card">
+    <div class="project-card-tag">Lloydsteven.com</div>
+    <img src="/{project}.png">
+  </div>
+</div>
+
+<style>
+  .drag-wrapper {
+    position: relative;
+    max-width: fit-content;
+  }
+  .project-card {
+    position: relative;
+    cursor: grab;
+    max-width: fit-content;
+    transition: transform 0.2s;
+    transform: rotateZ(var(--initialAngle));
+  }
+  .project-card:hover {
+    z-index: 3;
+    transform: scale(1.05) rotateZ(var(--initialAngle));
+    transform-origin: center;
+    transition: transform 0.1s;
+    transition-timing-function: ease;
+  }
+  .project-card:hover > .project-card-tag{
+    transform: translateY(-1px);
+    transform-origin: center;
+    transition: transform 0.2s;
+    transition-timing-function: ease;
+  }
+  img {
+    min-width: 500px;
+    pointer-events: none;
+    outline: 1px solid var(--dark-grey);
+    border: 1px solid var(--white);
+    box-shadow: 
+                0px 4px 4px 0px rgba(55,55,85,0.10),
+                0px 3px 4px 6px rgba(213,213,213,0.15);
+    display: block;
+    border-radius: 0px 2px 2px 2px;
+  }
+  .project-card-tag {
+    position: relative;
+    z-index: -1;
+    max-width: fit-content;
+    padding: 2px 8px;
+    border-radius: 2px 2px 0px 0px;
+
+    outline: 1px solid var(--dark-grey);    
+    border: 1px solid var(--white);
+    background: var(--light-grey);
+    color: var(--black);
+
+    font-family: "STIX Two Text", serif;
+    font-optical-sizing: auto;
+    font-style: normal;
+    letter-spacing: -0.03rem;
+    font-weight: 300;
+    
+    transform: translateY(calc(110% - 1px));
+    transition: transform 0.2s;
+    transition-timing-function: ease;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import ProjectCard from "$lib/project-card.svelte"
+  
+  const projects = [
+    { id: 'card1', project: 'lloydsteven', initialAngle: '-3.62deg' },
+    { id: 'card2', project: 'feel', initialAngle: '0deg' },
+  ];
+
+</script>
+
+<div class="container">
+  {#each projects as project}
+    <ProjectCard 
+      id={project.id} 
+      project={project.project} 
+      --initialAngle={project.initialAngle}
+    />
+  {/each}
+</div>
